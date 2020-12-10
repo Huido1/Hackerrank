cbee11f985495865b22b9ad49fd7aa6dcc1e2508
Huido1 <agustin1053@hotmail.com>
2020-12-10 16:55:36 -0300

    Create 06 - Write a function.py

---
+++ Python/Introduction/06 - Write a function.py
@@ -0,0 +1,10 @@
+#url: https://www.hackerrank.com/challenges/write-a-function/problem
+
+def is_leap(year):
+    if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+        return True
+    else:
+        return False
+        
+year = int(input())        #Estructura obligatoria por la pagina
+print(is_leap(year))
