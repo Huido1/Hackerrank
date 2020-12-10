fa1fb45f5d69e5d17e0e87ad8ad44408560a878b
Huido1 <agustin1053@hotmail.com>
2020-12-10 17:30:50 -0300

    Create 07 - Print Function.py

---
+++ Python/Introduction/07 - Print Function.py
@@ -0,0 +1,6 @@
+#url: https://www.hackerrank.com/challenges/python-print/problem
+
+n = int(input())
+
+for i in range (1, n+1):
+    print (i, end="")      #No olvidar el end="", ya que de esa manera evito la newline y cumplo el formato pedido
