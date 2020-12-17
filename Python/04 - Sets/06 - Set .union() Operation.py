c10b26ebd90976213a3b34f2e248305d305fcecd
Huido1 <agustin1053@hotmail.com>
2020-12-17 15:41:16 -0300

    Create 06 - Set .union() Operation.py

---
+++ Python/04 - Sets/06 - Set .union() Operation.py
@@ -0,0 +1,10 @@
+#url: https://www.hackerrank.com/challenges/py-set-union/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+N = set(map(int, input().split()))
+
+b = int(input())
+B = set(map(int, input().split()))
+
+print(len(N|B))
