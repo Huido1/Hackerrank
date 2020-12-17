8d518aa1ce307b3ab3586ff4a32af517e516ec47
Huido1 <agustin1053@hotmail.com>
2020-12-17 15:44:01 -0300

    Create 07 - Set .intersection() Operation.py

---
+++ Python/04 - Sets/07 - Set .intersection() Operation.py
@@ -0,0 +1,11 @@
+#url: https://www.hackerrank.com/challenges/py-set-intersection-operation/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+N = set(map(int, input().split()))
+
+b = int(input())
+B = set(map(int, input().split()))
+
+print(len(N&B))
