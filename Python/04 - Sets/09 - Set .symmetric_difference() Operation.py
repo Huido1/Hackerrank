3d5bf610f32bacf496ca45b2344853e4d40f1d67
Huido1 <agustin1053@hotmail.com>
2020-12-17 15:48:45 -0300

    Create 09 - Set .symmetric_difference() Operation.py

---
+++ Python/04 - Sets/09 - Set .symmetric_difference() Operation.py
@@ -0,0 +1,11 @@
+#url: https://www.hackerrank.com/challenges/py-set-symmetric-difference-operation/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+n = int(input())
+N = set(map(int, input().split()))
+
+b = int(input())
+B = set(map(int, input().split()))
+
+print(len(N^B))
