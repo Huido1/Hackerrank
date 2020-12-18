fd5a1efa9d06b4b10b1545e01e3a404ba0ee4b38
Huido1 <agustin1053@hotmail.com>
2020-12-18 17:00:26 -0300

    Create 06 - Integers Come In All Sizes.py

---
+++ Python/05 - Math/06 - Integers Come In All Sizes.py
@@ -0,0 +1,7 @@
+#https://www.hackerrank.com/challenges/python-integers-come-in-all-sizes/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a, b, c, d = (int(input()) for i in range(4))
+
+print((a**b+c**d))
