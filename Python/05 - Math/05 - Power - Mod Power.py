b1612fa852619d672aec9e4e4dccd1b021c472b5
Huido1 <agustin1053@hotmail.com>
2020-12-18 16:55:09 -0300

    Create 05 - Power - Mod Power.py

---
+++ Python/05 - Math/05 - Power - Mod Power.py
@@ -0,0 +1,10 @@
+#url: https://www.hackerrank.com/challenges/python-power-mod-power/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(input())
+b = int(input())
+m = int(input())
+
+print(a**b)
+print(pow(a,b,m))
