fea2db577229d8b0858ab63527add2d04c3da550
Huido1 <agustin1053@hotmail.com>
2020-12-19 17:13:56 -0300

    Create 05 - Compress the String!.py

---
+++ Python/06 - Itertools/05 - Compress the String!.py
@@ -0,0 +1,5 @@
+#url: https://www.hackerrank.com/challenges/compress-the-string/problem
+
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+from itertools import groupby
+print(*[(len(list(c)), int(k)) for k, c in groupby(input())])
